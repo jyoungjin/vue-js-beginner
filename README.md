c8739e8d24eb9cecf4010aa63931f9f12d50d585
jyoungjin <ccd1202@naver.com>
2024-04-01 18:32:27 +0900

    docs: vue form 학습

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ---
 
+- [Vue 공식 사이트](https://vuejs.org/)
+- Reactivity
+  - 데이터에 대한 변경을 감지해서 반영하는 것
 - 지역 컴포넌트 vs 전역 컴포넌트
   - 상위에서 하위로는 데이터를 내려줌, 프롭스 속성 (props)
   - 하위에서 상위로는 이벤트를 올려줌, 이벤트 발생 (emit)
@@ -15,9 +18,13 @@
   - async & await
 - 템플릿 문법
   - {{ data }}
-  - v-bind, v-if, v-show, v-on(keyup, keypress)
+  - 뷰 디렉티브 (v-)
+    - v-bind
+    - v-if
+    - v-show
+    - v-on(keyup, keypress)
   - watch 속성 vs computed 속성
     - watch: 무거운 로직
     - computed: validation이나 가벼운 연산 로직
 - Vue CLI
-  - [공식 사이트](https://cli.vuejs.org/)
+  - [Vue CLI 공식 사이트](https://cli.vuejs.org/)
--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  <!-- <div>{{ str }}</div> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: "App",
+  data: function () {
+    return {
+      str: "Header",
+    };
+  },
   components: {
-    HelloWorld,
+    "app-header": AppHeader,
+  },
+  methods: {
+    renewStr: function () {
+      this.str = "HI";
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
